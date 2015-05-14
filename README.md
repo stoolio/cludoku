bc26b38de0ca8a1719300089071ce83ce0c112a2
Steven Migliorato <smigliorato@gmail.com>
2015-05-14 03:10:18 -0500

    basics set-based calculation of possible grid values, README updated

--- README.md
+++ README.md
@@ -1,28 +1,44 @@
 # cludoku
 
-FIXME: description
+A project to solve sudoku puzzles, but mainly to learn a bit of clojure. My brain already hurts.
 
 ## Installation
 
-Download from http://example.com/FIXME.
+Download from the page you are most likely reading this on (github.com/stoolio/cludoku).
 
 ## Usage
 
-FIXME: explanation
+There are a few utility funtions that can be used. If you run:
+
+    $ lein repl
+    ...
+    => (require 'cludoku.core)
+    => (cludoku.core/-main)
+
+You will get an ugly printout of the what is possible for each square.
+
+For whatever reason, `lein run` doesn't output anything. I will be looking into that.
+
+Some day, I will bundle this up in a jar and you will be able to use the command below. That day is not today.
 
     $ java -jar cludoku-0.1.0-standalone.jar [args]
 
 ## Options
 
-FIXME: listing of options this app accepts.
+None yet. The puzzle is current hardcoded, so if you want to ~~solve a different puzzle~~ get an unformatted blob of possibilities based on an incomplete algorithm, you have to edit `puzzle.txt` in the root.
 
 ## Examples
 
-...
+```
+(def puzzle
+  (load-puzzle "puzzle.txt"))
+
+(possibles puzzle 3 2) ; => #{3 2 9 5}
+```
 
 ### Bugs
 
-...
+There are no tests whatsoever. In fact the only tests there are were created by `lein new app` and are failing. That sounds like a double failure.
 
 ### Any Other Sections
 ### That You Think
--- src/cludoku/core.clj
+++ src/cludoku/core.clj
@@ -1,7 +1,90 @@
 (ns cludoku.core
   (:gen-class))
 
+(use 'clojure.set)
+
+(defn parse-row
+  "turns raw puzzle data into useful data"
+    [x]
+    (->> (clojure.string/split x #" ")
+      (mapv read-string)
+      (mapv (fn [x] (if (= x '_) nil x)))))
+
+(defn load-puzzle
+  "Reads a sudoku puzzle in a specified format"
+  [filename]
+  (with-open [rdr (clojure.java.io/reader filename)]
+    (->> (line-seq rdr)
+      (reduce conj [])
+      (mapv parse-row))))
+
+(defn row
+  [puzzle y]
+  (get puzzle y))
+
+(defn get-sq
+  [puzzle x y]
+  (get (get puzzle y) x))
+
+(defn col
+  [puzzle x]
+  (let [coords (for [y (range 0 9)] [x y])]
+    (map (fn [x] (apply get-sq (concat [puzzle] x))) coords)))
+
+(defn grid
+  "returns a col first 3x3 grid, coords are in (range 0 3)"
+  [puzzle _x _y]
+  (let [ -x (* 3 (int (/ _x 3)))
+         -y (* 3 (int (/ _y 3)))
+         coords (for [x (range -x (+ -x 3)) y (range -y (+ -y 3))] [x y])]
+    (map (fn [x]
+      (apply get-sq (concat [puzzle] x))) coords)))
+
+(defn present-numbers
+  [group]
+  (set (filter (fn [x] (not (nil? x))) group)))
+
+(def all-numbers
+  (set (range 1 10)))
+
+(defn possibles 
+  [puzzle x y]
+  (if (not (nil? (get-sq puzzle x y)))
+    (get-sq puzzle x y)
+    (let [ _col (present-numbers (col puzzle x))
+           _row (present-numbers (row puzzle y))
+           _grid (present-numbers (grid puzzle x y))
+           all-present (clojure.set/union _col _row _grid)]
+      (clojure.set/difference all-numbers all-present))))
+
+; quick algo idea:
+; go through each col/row/grid
+; for each, generate a set of possibilities for each square
+; by finding whats available via difference from (set range(1 10))
+; then, we will have 3 sets of for each unknown square
+; the intersection of those sets are the possibilities
+; after that, cry if it isn't solved
+; EDIT: possibles calculates whatever is in the realm of possibility
+; I need to write some unit test of the various functions, I made a few
+; mistakes and it would have made my dev process much easier
+
+(def puzzle
+  (load-puzzle "puzzle.txt"))
+
+(def kim-possible
+  (partial possibles puzzle))
+
+(def all-coords
+  (for [x (range 0 9) y (range 0 9)] [x y]))
+
+; Make a better display func
+(->> all-coords
+  (map (fn [x] (apply kim-possible x)))
+  (map println))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (->> all-coords
+    (map (fn [x] (apply kim-possible x)))
+    (map println)))
